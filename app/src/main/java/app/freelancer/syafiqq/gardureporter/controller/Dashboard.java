c16a27961ff4736db9ff3ec39b1241388fbba7ec
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-04 07:03:54 +0700

    Fix LatLon based on [Stack Overflow!](https://stackoverflow.com/questions/41310624/android-how-to-get-current-location-in-longitude-and-latitude)

--- app/src/main/java/app/freelancer/syafiqq/gardureporter/controller/Dashboard.java
+++ app/src/main/java/app/freelancer/syafiqq/gardureporter/controller/Dashboard.java
@@ -1,14 +1,70 @@
 package app.freelancer.syafiqq.gardureporter.controller;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.ErrorDialogFragment;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import app.freelancer.syafiqq.gardureporter.R;
 
-public class Dashboard extends AppCompatActivity
+public class Dashboard extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, LocationListener, GoogleApiClient.ConnectionCallbacks
 {
+    // Request code to use when launching the resolution activity
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    // Unique tag for the error dialog fragment
+    private static final String DIALOG_ERROR = "dialog_error";
+    // Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private double latitude;
+    private double longitude;
+    private LocationRequest mLocationRequest;
+
+    public static boolean isInternetConnected(Context ctx)
+    {
+        ConnectivityManager connectivityMgr = (ConnectivityManager) ctx
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobile = connectivityMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        // Check if wifi or mobile network is available or not. If any of them is
+        // available or connected then it will return true, otherwise false;
+        if(wifi != null)
+        {
+            if(wifi.isConnected())
+            {
+                return true;
+            }
+        }
+        if(mobile != null)
+        {
+            if(mobile.isConnected())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -17,23 +73,170 @@ public class Dashboard extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        GetCurrentGPSLocation gps = new GetCurrentGPSLocation(this);
+        this.mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .setAccountName("syafiq.rezpector@gmail.com")
+                .build();
 
-        // check if GPS enabled
-        if(gps.canGetLocation())
+        this.mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(30000); //5 seconds
+        mLocationRequest.setFastestInterval(30000); //3 seconds
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        //mLocationRequest.setSmallestDisplacement(0.1F); //1/10 meter
+
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
-            double latitude = gps.getLatitude();
-            double longitude = gps.getLongitude();
-            Toast.makeText(this, "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+            return;
+        }
+        this.locationListener = new LocationListener()
+        {
+            @Override
+            public void onLocationChanged(Location location)
+            {
+                Dashboard.this.onLocationChanged(location);
+            }
+        };
+    }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult result)
+    {
+        if(mResolvingError)
+        {
+            // Already attempting to resolve an error.
+            return;
+        }
+        else if(result.hasResolution())
+        {
+            try
+            {
+                mResolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            }
+            catch(IntentSender.SendIntentException e)
+            {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+            }
         }
         else
         {
-            // can't get location
-            // GPS or Network is not enabled
-            // Ask user to enable GPS/network in settings
-            gps.showSettingsAlert();
+            // Show dialog using GoogleApiAvailability.getErrorDialog()
+            showErrorDialog(result.getErrorCode());
+            mResolvingError = true;
         }
     }
 
+    private void showErrorDialog(int errorCode)
+    {
+        // Create a fragment for the error dialog
+        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+        // Pass the error that should be displayed
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(super.getFragmentManager(), "errordialog");
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        if(isGPSEnabled(this))
+        {
+            if(locationListener != null)
+            {
+                if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                {
+                    return;
+                }
+
+                if(locationManager != null)
+                {
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if(location != null)
+                    {
+                        Dashboard.this.latitude = location.getLatitude();
+                        Dashboard.this.longitude = location.getLongitude();
+                        Log.d("Dashboard", "GPS Your Location is - \nLat: " + latitude + "\nLong: " + longitude);
+                    }
+                }
+            }
+        }
+
+        if(isInternetConnected(this))
+        {
+            if(locationManager != null)
+            {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if(location != null)
+                {
+                    Dashboard.this.latitude = location.getLatitude();
+                    Dashboard.this.longitude = location.getLongitude();
+                    Log.d("Dashboard", "Network Your Location is - \nLat: " + latitude + "\nLong: " + longitude);
+                }
+            }
+        }
+
+
+    }
+
+    public void onProviderDisabled(String provider)
+    {
+
+    }
+
+    public void onProviderEnabled(String provider)
+    {
+
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+
+    }
+
+    protected void onStart()
+    {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop()
+    {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    public boolean isGPSEnabled(Context mContext)
+    {
+        LocationManager locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle)
+    {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this.locationListener);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i)
+    {
+
+    }
 }
