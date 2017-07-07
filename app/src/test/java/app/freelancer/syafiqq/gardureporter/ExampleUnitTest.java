a4d9100923796f302d9c1c6c637be403733318de
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-07 09:30:50 +0700

    Reformat code

--- app/src/test/java/app/freelancer/syafiqq/gardureporter/ExampleUnitTest.java
+++ app/src/test/java/app/freelancer/syafiqq/gardureporter/ExampleUnitTest.java
@@ -1,8 +1,7 @@
 package app.freelancer.syafiqq.gardureporter;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
