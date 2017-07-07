3c0ff15fcf8ffca41051bcdbb0adb9408bf57797
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-07 07:29:04 +0700

    Add initial Commit for SplashScreen

---
+++ app/src/main/java/app/freelancer/syafiqq/gardureporter/controller/SplashScreen.java
@@ -0,0 +1,15 @@
+package app.freelancer.syafiqq.gardureporter.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.freelancer.syafiqq.gardureporter.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
