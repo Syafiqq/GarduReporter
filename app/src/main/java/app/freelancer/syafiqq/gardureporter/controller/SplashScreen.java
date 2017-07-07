77a2e4008f8d6909aaa9ceba252f7c4e1703a3cb
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-07 09:33:17 +0700

    Add authentication Jumper

--- app/src/main/java/app/freelancer/syafiqq/gardureporter/controller/SplashScreen.java
+++ app/src/main/java/app/freelancer/syafiqq/gardureporter/controller/SplashScreen.java
@@ -1,11 +1,16 @@
 package app.freelancer.syafiqq.gardureporter.controller;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import app.freelancer.syafiqq.gardureporter.BuildConfig;
 import app.freelancer.syafiqq.gardureporter.R;
+import app.freelancer.syafiqq.gardureporter.model.util.Setting;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import timber.log.Timber;
 
 public class SplashScreen extends AppCompatActivity
@@ -13,19 +18,35 @@ public class SplashScreen extends AppCompatActivity
 
     private static final int SECONDS_DELAYED = 2;
 
+    @Nullable private Setting setting;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_splash_screen);
 
+        this.setting = Setting.getOurInstance();
+
         this.initializeTimber();
         new AsyncTask<Void, Void, Void>()
         {
+            private final int NEED_AUTH = 0x00;
+
+            private int state = NEED_AUTH;
+
             @Override
             protected Void doInBackground(Void... voids)
             {
-
+                SharedPreferences settings = SplashScreen.super.getSharedPreferences(Setting.SharedPreferences.SHARED_PREFERENCES_AUTHENTICATION, MODE_PRIVATE);
+                if(settings.contains(SplashScreen.super.getResources().getString(R.string.shared_preferences_authentication_token)))
+                {
+                    Timber.d("Yes Token");
+                }
+                else
+                {
+                    this.state = NEED_AUTH;
+                }
                 return null;
             }
 
@@ -36,8 +57,13 @@ public class SplashScreen extends AppCompatActivity
                 {
                     public void run()
                     {
-                        //SplashScreen.super.startActivity(new Intent(SplashScreen.this, Dashboard.class));
-                        //SplashScreen.super.finish();
+                        if(state == NEED_AUTH)
+                        {
+                            @NotNull Intent intent = new Intent(SplashScreen.this, AuthLogin.class);
+                            intent.putExtra(Setting.Jumper.NAME, Setting.Jumper.CLASS_DASHBOARD);
+                            SplashScreen.super.startActivity(intent);
+                            SplashScreen.super.finish();
+                        }
                     }
                 }, SECONDS_DELAYED * 1000);
             }
