7fefdcb3da9db9d5e03d2528cc5fbada170256b2
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-07 20:51:50 +0700

    Initial Commit of Token

---
+++ app/src/main/java/app/freelancer/syafiqq/gardureporter/model/util/Token.java
@@ -0,0 +1,13 @@
+package app.freelancer.syafiqq.gardureporter.model.util;
+
+/*
+ * This <GarduReporter> created by : 
+ * Name         : syafiq
+ * Date / Time  : 07 July 2017, 8:50 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+public class Token
+{
+}
