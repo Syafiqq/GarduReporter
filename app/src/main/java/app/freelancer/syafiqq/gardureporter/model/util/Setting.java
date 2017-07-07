d687c6adea392e448b128eb69f556fe9ea350bd8
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-07 09:33:01 +0700

    Add shared preferences and Jumper

--- app/src/main/java/app/freelancer/syafiqq/gardureporter/model/util/Setting.java
+++ app/src/main/java/app/freelancer/syafiqq/gardureporter/model/util/Setting.java
@@ -1,41 +1,65 @@
 package app.freelancer.syafiqq.gardureporter.model.util;
 
-/**
- * Created by syafiq on 6/23/17.
+/*
+ * This <GarduReporter> created by :
+ * Name         : syafiq
+ * Date / Time  : 07 July 2017, 8:14 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
  */
 
-public class Setting {
+public class Setting
+{
     private static final Setting ourInstance = new Setting();
 
     private Networking networking;
 
-    static Setting getInstance() {
-        return ourInstance;
-    }
-
-    private Setting() {
+    private Setting()
+    {
         this.networking = new Networking();
 
         this.networking.setDomain("http://freelancer.gardu.reporter.app");
     }
 
-    public static Setting getOurInstance() {
+    static Setting getInstance()
+    {
         return ourInstance;
     }
 
-    public Networking getNetworking() {
+    public static Setting getOurInstance()
+    {
+        return ourInstance;
+    }
+
+    public Networking getNetworking()
+    {
         return networking;
     }
 
-    public static class Networking {
+    public static class Networking
+    {
         private String domain;
 
-        public String getDomain() {
+        public String getDomain()
+        {
             return domain;
         }
 
-        public void setDomain(String domain) {
+        public void setDomain(String domain)
+        {
             this.domain = domain;
         }
+    }
+
+    public static class SharedPreferences
+    {
+        public static final String SHARED_PREFERENCES_AUTHENTICATION = "SHARED_PREFERENCE_AUTHENTICATION";
+    }
+
+    public static class Jumper
+    {
+        public static final String NAME = "jumper";
+        public static final int CLASS_DASHBOARD = 0x01;
+        public static final int CLASS_AUTH_LOGIN = 0x02;
     }
 }
