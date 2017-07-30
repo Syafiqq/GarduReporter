35f93d8ae31dc713e10fb75b002bdf886c87e2e5
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-30 18:55:45 +0700

    Add logging

--- app/src/main/java/app/freelancer/syafiqq/gardureporter/controller/AuthLogin.java
+++ app/src/main/java/app/freelancer/syafiqq/gardureporter/controller/AuthLogin.java
@@ -202,14 +202,14 @@ public class AuthLogin extends AppCompatActivity implements AuthDao.LoginRequest
 
     @Override public void onLoginFailed(int status, String message)
     {
-        Timber.d("onLoginFailed");
+        Timber.d("onRequestFailed");
 
         this.postLogin(false, message);
     }
 
     @Override public void onLoginSuccessful(TokenOrm token, int status, String message)
     {
-        Timber.d("onLoginSuccessful");
+        Timber.d("onRequestSuccessful");
 
         TokenDao.storeToken(this.getApplicationContext(), token);
         this.postLogin(status > 0, message);
