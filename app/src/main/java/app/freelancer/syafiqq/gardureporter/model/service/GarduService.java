a4b9bb85c5d7a824aebd221ad5c80abdb8b7636a
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-30 18:57:17 +0700

    Add gardu service

---
+++ app/src/main/java/app/freelancer/syafiqq/gardureporter/model/service/GarduService.java
@@ -0,0 +1,19 @@
+package app.freelancer.syafiqq.gardureporter.model.service;
+
+/*
+ * This <GarduReporter> created by : 
+ * Name         : syafiq
+ * Date / Time  : 30 July 2017, 8:51 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GarduService
+{
+    @GET("/api/mobile/gardu/induk/find?code=C41AF")
+    Call<ResponseBody> findGarduInduk();
+}
