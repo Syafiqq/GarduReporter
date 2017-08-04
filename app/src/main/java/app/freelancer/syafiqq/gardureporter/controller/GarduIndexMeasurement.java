f38cf97a01b8ae0f5b55cd82c79d32a9ce48f47e
Syafiqq <syafiq.rezpector@gmail.com>
2017-08-04 07:41:36 +0700

    Finishing layout

--- app/src/main/java/app/freelancer/syafiqq/gardureporter/controller/GarduIndexMeasurement.java
+++ app/src/main/java/app/freelancer/syafiqq/gardureporter/controller/GarduIndexMeasurement.java
@@ -3,10 +3,7 @@ package app.freelancer.syafiqq.gardureporter.controller;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.CompoundButton;
-import android.widget.Switch;
 import app.freelancer.syafiqq.gardureporter.R;
-import com.fenjuly.mylibrary.ToggleExpandLayout;
 
 public class GarduIndexMeasurement extends AppCompatActivity
 {
@@ -18,24 +15,6 @@ public class GarduIndexMeasurement extends AppCompatActivity
         setContentView(R.layout.activity_gardu_index_measurement);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        final ToggleExpandLayout layout = (ToggleExpandLayout) findViewById(R.id.toogleLayout);
-        Switch switchButton = (Switch) findViewById(R.id.switch_button);
-        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            @Override public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked)
-            {
-                if(isChecked)
-                {
-                    layout.open();
-                }
-                else
-                {
-                    layout.close();
-                }
-            }
-        });
-
     }
 
 }
