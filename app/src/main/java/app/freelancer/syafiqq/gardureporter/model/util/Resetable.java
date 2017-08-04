f222efe8e5dca631647a4f1b60fa0a52093dadd1
Syafiqq <syafiq.rezpector@gmail.com>
2017-08-04 11:28:50 +0700

    Add resetable interface

---
+++ app/src/main/java/app/freelancer/syafiqq/gardureporter/model/util/Resetable.java
@@ -0,0 +1,14 @@
+package app.freelancer.syafiqq.gardureporter.model.util;
+
+/*
+ * This <GarduReporter> created by : 
+ * Name         : syafiq
+ * Date / Time  : 04 August 2017, 9:24 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+public interface Resetable
+{
+    void reset();
+}
